3b6db9319b1448a8991a53ed1029a160b92f4558
Makson Vinicio <maksonvinicio7@gmail.com>
2020-07-23 10:13:03 -0300

    Create 014.py

---
+++ 1. Variaveis/014.py
@@ -0,0 +1,3 @@
+#Conversor de Temperaturas
+temperatura = float(input("Entre com uma temperatura em Celsius: "))
+print(f"A temperatura de {temperatura} °C em Fahrenheit, e de {(temperatura * 9/5) + 32} °F")
