009eb3a4c9993373a873f7ec438e69bc9878f8ae
Makson Vinicio <maksonvinicio7@gmail.com>
2020-07-30 17:03:27 -0300

    Update 038.py

---
+++ 2. Condicionais/038.py
@@ -0,0 +1,8 @@
+# Comparando números
+n1 = int(input("Primeiro numero"))
+n2 = int(input("Segundo numero"))
+if n1 > n2:
+    print("O primeiro numero e maior")
+elif:
+    n1 < n2:
+    print("O segundo numero e maior")
