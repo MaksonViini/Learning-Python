86a8d12b59c748bc90744cada1d9b28192277b8d
Makson Vinicio <maksonvinicio7@gmail.com>
2020-07-31 11:26:25 -0300

    Update 040.py

---
+++ 2. Condicionais/040.py
@@ -0,0 +1,10 @@
+# Aquele clássico da Média
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+media = (n1 + n2) / 2
+if media >= 6:
+    print(f"A media do aluno e {media}")
+    print("Aprovado!!!")
+else:
+    print(f"A media do aluno e {media}")
+    print("Reprovado")
