b2cb5caf1dcda5f6def02824ea31ac36a1b3b06d
Makson Vinicio <maksonvinicio7@gmail.com>
2020-08-02 18:12:09 -0300

    Update 52.py

---
+++ 3. Repeticao/52.py
@@ -0,0 +1,10 @@
+# Números primos
+num = int(input("Digite um numero: "))
+cont = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        cont += 1
+if cont == 2:
+    print(f"O numero {num} e primo, pois e divisivel {cont} vezes ")
+else:
+    print(f"O numero {num} nao e primo, pois e divisivel {cont} vezes")
