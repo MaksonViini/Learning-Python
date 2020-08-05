7bd25a708b627dce8edfa346de989de08af85366
Makson Vinicio <maksonvinicio7@gmail.com>
2020-08-05 11:21:08 -0300

    Update 62.py

---
+++ 3. Repeticao/62.py
@@ -0,0 +1,8 @@
+# Super Progressão Aritmética v3.0
+print("=" * 20)
+print("10 termos de uma pa".upper())
+print("=" * 20)
+termo = int(input("Primeiro termo: "))
+razao = int(input("Razao: "))
+cont = 0
+while 
