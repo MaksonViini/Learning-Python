959d02cf293b0418a49d59ce325f4e84cd73ae8d
Makson Vinicio <maksonvinicio7@gmail.com>
2020-08-07 16:23:42 -0300

    Update 62.py

--- 3. Repeticao/62.py
+++ 3. Repeticao/62.py
@@ -1,8 +1,25 @@
 # Super Progressão Aritmética v3.0
 print("=" * 20)
-print("10 termos de uma pa".upper())
+print("gerador de pa".upper())
 print("=" * 20)
 termo = int(input("Primeiro termo: "))
 razao = int(input("Razao: "))
-cont = 0
-while 
+cont = 10
+i = 0
+k = x = 0
+while True:
+    if i != cont:
+        i += 1
+        print(termo,"->", end=" ")
+        termo += razao
+        x += 1
+    elif i != 0:
+        print("PAUSE".upper())
+        termo1 = int(input("\nQuantos termos voce quer mostrar a mais? "))
+        cont = termo1
+        termo1 += razao
+        i = 0
+        k += 1
+    elif cont == 0:
+        break
+print(f"Progressao finalizada com {(x + k) - 2} termos ")
