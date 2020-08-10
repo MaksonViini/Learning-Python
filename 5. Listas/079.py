73a9cb06d0a77cd1b81102f64fd237a3edb56a52
Makson Vinicio <maksonvinicio7@gmail.com>
2020-08-10 16:08:07 -0300

    Update 079.py

--- 5. Listas/079.py
+++ 5. Listas/079.py
@@ -1,1 +1,14 @@
 # Valores únicos em uma Lista
+lista = []
+while True:
+    n = int(input("Digite um valor: "))
+    lista.append(n)
+    if lista.count(n) == 2:
+        print("Valor duplicado! Nao vai ser adicionado")
+        lista.pop()
+    else:
+        print("Valor adicionado com sucesso: ")
+    op = str(input("Quer continuar [S/N] "))
+    if op in 'Nn':
+        break
+print(sorted(lista))
