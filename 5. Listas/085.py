1ea1557734f40c1d9d0e45f85ed636a95ed62944
Makson Vinicio <maksonvinicio7@gmail.com>
2020-08-11 14:44:07 -0300

    Update 085.py

---
+++ 5. Listas/085.py
@@ -0,0 +1,10 @@
+# Listas com pares e ímpares
+lista= [[], []]
+for i in range(1, 7):
+    num = int(input(f"Digite o {i}º valor: "))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+print(f"Os valores pares digitados foram {sorted(lista[0])} ")
+print(f"Os valores impares digitados foram {sorted(lista[1])} ")
