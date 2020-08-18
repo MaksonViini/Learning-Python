fc02bb367aab095cfd5e245912f692112de12bb2
MaksonViini <maksonvinicio7@gmail.com>
2020-08-17 21:25:57 -0300

    Update 106.py

--- 7. Funcoes/106.py
+++ 7. Funcoes/106.py
@@ -1,1 +1,16 @@
-dsds
+# Sistema interativo de ajuda em Python
+from time import sleep
+def ajuda (texto):
+    print(f'Acessando o manual do comando {texto} ')
+    sleep(0.5)
+    help(texto)
+
+while True:
+    print('--' * 12)
+    print(' Sistema de ajuda PyHelp')
+    print('--' * 12)
+    f = str(input("Funcao ou Biblioteca > "))
+    if f in 'FIMfim':
+        break
+    else:
+        ajuda(f)
