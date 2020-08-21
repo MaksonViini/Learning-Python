3c04eda94888c05657ee296e37a5d4b2f187c247
MaksonViini <maksonvinicio7@gmail.com>
2020-08-20 21:45:30 -0300

    Att

---
+++ 7. Funcoes/celsius2fahrenheit.py
@@ -0,0 +1,3 @@
+def celsius2fahrenheit (graus):
+    return graus * 9/5 + 32
+print(celsius2fahrenheit(6))
---
+++ 7. Funcoes/numero_par.py
@@ -0,0 +1,6 @@
+def numero_par (num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+print(numero_par(4))
