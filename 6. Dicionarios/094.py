3a16bda70d84e060bf6a602cfd9c8c5c25285e33
MaksonViini <maksonvinicio7@gmail.com>
2020-09-02 20:48:31 -0300

    Update 094.py

--- 6. Dicionarios/094.py
+++ 6. Dicionarios/094.py
@@ -1,7 +1,7 @@
 # Unindo dicionários e listas
 dic = {'Nome': '', 'Sexo': '', 'Idade': ''}
-#dic = {'Nome': str(input('Nome: ')), 'Sexo': str(input('Sexo: ')), 'Idade': int(input('Idade: '))}
 lista = []
+idade = []
 while True:
     dic['Nome'] =  str(input('Nome: ')),
     dic['Sexo'] =  str(input('Sexo: '))
@@ -9,16 +9,25 @@ while True:
         print('ERRO! Por favor digite somente M ou F')
         Sexo =  str(input('Sexo: '))
     dic['Idade'] =  int(input('Idade: '))
+    idade.append(dic['Idade'])
     lista.append(dic)
     op = str(input('Quer continuar ? [S/N]: '))
     if op in 'Nn':
         break
-    else:
+    elif op not in 'NnSs':
         print('ERRO! Por favor digite somente S ou N')
         op = str(input('Quer continuar ? [S/N]: '))
 print(f'A) Ao todo temos {len(lista)} pessoas cadastradas: ')
-print(f'B) A media de idade e {sum(lista[1]) /(len(lista))}: ')
-#print(f'C) Ao todo temos {len(lista)} pessoas cadastradas: ')
-#print(f'D) Ao todo temos {len(lista)} pessoas cadastradas: ')
-print(dic)
-
+media = sum(idade) / (len(idade))
+print(f'B) A media de idade e {media}: ')
+print(f'C) As mulheres cadastradas foram: ')
+for p in lista:
+    if p['Sexo'] in 'Ff':
+        print(p['Nome'], end = '')
+print()
+print(lista[0])
+print(f'C) Lista das pessoas que estao com idade acima da media: ')
+for p in lista:
+    if p['Idade'] > media:
+        print(p['Idade'], end = ' ')
+print()
