faffd368a2ca279ebed6609f3babbd0a9f8f9f71
MaksonViini <maksonvinicio7@gmail.com>
2020-11-20 13:00:40 -0300

    Update 105.py

--- 7. Funcoes/105.py
+++ 7. Funcoes/105.py
@@ -1,11 +1,11 @@
 # Analisando e gerando Dicionários
 from statistics import mean
-def notas (*notas, sit = False):
+def notas(*notas, sit = False):
     dic = {'Total':len(notas), 'Maior': max(notas), 'Menor': min(notas), 'Media': mean(notas)}
     if sit == True:
         if dic['Media'] < 6:
             dic['Situacao'] = 'Ruim'
-        elif dic['Media'] >= 6 and dic['Media'] <= 7:
+        elif dic['Media'] <= 7:
             dic['Situacao'] = 'Razoavel'
         else:
             dic['Situacao'] = 'Boa'
