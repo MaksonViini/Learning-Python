216bd75a32a59f76d8a074931bcc509e937149e5
MaksonViini <maksonvinicio7@gmail.com>
2020-11-20 13:00:42 -0300

    Update 104.py

--- 7. Funcoes/104.py
+++ 7. Funcoes/104.py
@@ -1,11 +1,9 @@
 # Validando entrada de dados em Python
-def leia_int (num):
+def leia_int(num):
     while True:
         if num.isnumeric():
             return num
-            break
-        else:
-            print("ERRO!, Numero invalido!")
-            num = str(input("Digite um numero: "))
+        print("ERRO!, Numero invalido!")
+        num = str(input("Digite um numero: "))
 n = leia_int(str(input("Digite um numero: ")))
 print(f"Voce digitou o numero {n} ")
