607fb60981ae901ba38857508deaa3f8d7961031
MaksonViini <maksonvinicio7@gmail.com>
2020-11-22 11:46:26 -0300

    Update 51.py

--- 3. Repeticao/51.py
+++ 3. Repeticao/51.py
@@ -4,7 +4,7 @@ print("10 termos de uma pa".upper())
 print("=" * 20)
 termo = int(input("Primeiro termo: "))
 razao = int(input("Razao: "))
-for i in range(1, 11):
+for _ in range(1, 11):
     print(termo,"->", end=" ")
     termo += razao
 print("acabou".upper())
