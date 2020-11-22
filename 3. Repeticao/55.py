c61dfe118905641dddf88282e0944a95e09c91aa
MaksonViini <maksonvinicio7@gmail.com>
2020-11-22 11:46:30 -0300

    Update 55.py

--- 3. Repeticao/55.py
+++ 3. Repeticao/55.py
@@ -3,10 +3,8 @@ maior = 0
 menor = 0
 for i in range(1, 4):
     peso = float(input(f"Peso da {i}ª pessoa: "))
-    if maior < peso:
-        maior = peso
+    maior = max(maior, peso)
     menor = peso
-    if menor >= peso:
-        menor = peso
+    menor = min(menor, peso)
 print(f"O maior peso e {maior}Kg")
 print(f"O menor peso e {menor}Kg")
