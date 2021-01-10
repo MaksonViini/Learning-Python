3b89366108794364e1afb2f3e364fad68c912a4a
MaksonViini <maksonvinicio7@gmail.com>
2021-01-10 17:24:28 -0300

    Update stack.py

--- Data Structures/stack.py
+++ Data Structures/stack.py
@@ -1,6 +1,37 @@
-#Create a structure stack
+'''
+Author: Makson Vinicio
+
+stack
+'''
 
 class Stack:
-    def __init__(self, last):
-        self.last = last
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
 
+    def push(self, item):
+        self.items.insert(0, item)
+    
+    def pop(self):
+        return self.items.pop(0)
+    
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+
+
+
+s = Stack()
+s.push('hello')
+s.push('true')
+s.push(4)
+s.push(7)
+#print(s.pop())
+#print(s.peek())
+#print(s.size())
+while not s.is_empty():
+    print(s.pop)
