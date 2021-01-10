9769cf54631a5d679e56572ccab6c9141936639e
MaksonViini <maksonvinicio7@gmail.com>
2021-01-10 18:08:01 -0300

    Estrutura de dados

---
+++ Data Structures/queue.py
@@ -0,0 +1,31 @@
+'''
+Author: Makson Vinicio
+
+Queue
+'''
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items())
+
+queue = Queue()
+
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+
+while queue.is_empty() != True:
+    print(queue.dequeue())
