e00003429ad902e8d5b42f79effeac839d4e3a6d
MaksonViini <maksonvinicio7@gmail.com>
2021-02-15 23:00:35 -0300

    Change name

---
+++ 01. Variable/007.py
@@ -0,0 +1,4 @@
+#Média Aritmética
+n1 = float(input("Digite a primeira nota do aluno: "))
+n2 = float(input("Digite a segunda nota do aluno: "))
+print(f"A media entre {n1:.1f} e {n2:.1f} e igual a {(n1 + n2) / 2:.1f} ")
