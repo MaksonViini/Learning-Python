43c58317384aa443a2477913060b7916b7004fd6
MaksonViini <maksonvinicio7@gmail.com>
2021-02-17 13:24:08 -0300

    Delete 006.py

--- 01. Variaveis/006.py
+++
@@ -1,6 +0,0 @@
-#Dobro, Triplo, Raiz Quadrada
-from math import sqrt
-n = int(input("Digite um numero: "))
-print(f"O dobro de {n} vale {n * 2}. ")
-print(f"O triplo de {n} vale {n * 3}. ")
-print(f"A raiz quadrada de {n} vale {sqrt(n):.2f}. ")
