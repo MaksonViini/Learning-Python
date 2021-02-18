0f49cc3b6cc287d37508d59a96173c604d3f6b20
MaksonViini <maksonvinicio7@gmail.com>
2021-02-18 17:10:50 -0300

    Delete 026.py

--- 01. Variaveis/026.py
+++
@@ -1,5 +0,0 @@
-#Primeira e última ocorrência de uma string
-frase = str(input("Digite uma frase: ")).upper().strip()
-print(f"A letra A aparece {frase.count('A')} vezes na frase ")
-print(f"A primeira letra A apareceu na posicao {frase.find('A') + 1}")
-print(f"A ultima letra A apareceu na posicao {frase.rfind('A') + 1} ")
