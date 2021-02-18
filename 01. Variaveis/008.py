92860e1dd0ab9d218e88d8f264bff58aca8c7bbd
MaksonViini <maksonvinicio7@gmail.com>
2021-02-18 17:10:43 -0300

    Delete 008.py

--- 01. Variaveis/008.py
+++
@@ -1,3 +0,0 @@
-#Conversor de Medidas
-metros = float(input("Uma medida em metros: "))
-print(f"A medida de {metros} m\n corresponde a {metros / 1000:.3f} km\n {metros / 100:.2f} hm\n {metros / 10:.1f} dam\n {metros * 10:.0f} dm\n {metros * 100:.0f} cm\n {metros * 1000:.0f} mm \n")
