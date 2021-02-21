7a192f0167663bf73ac1f79faf9753e8a4ee4693
MaksonViini <maksonvinicio7@gmail.com>
2021-02-21 13:48:06 -0300

    Delete 032.py

--- 02. Condicionais/032.py
+++
@@ -1,9 +0,0 @@
-# Ano Bissexto
-from datetime import date
-ano = int(input("Quer analisar qual ano? Digite 0 para analisar o ano atual: "))
-if ano == 0:
-    ano = date.today().year
-if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print(f"O ano {ano} e bissexto")
-else:
-    print(f"O ano {ano} nao e bissexto ")
