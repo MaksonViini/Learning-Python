9cfc1455ac1078e30aa4f8fb7cdadec027277e19
MaksonViini <maksonvinicio7@gmail.com>
2021-02-20 21:25:31 -0300

    Delete 014.py

--- 01. Variaveis/014.py
+++
@@ -1,3 +0,0 @@
-#Conversor de Temperaturas
-temperatura = float(input("Entre com uma temperatura em Celsius: "))
-print(f"A temperatura de {temperatura} °C em Fahrenheit, e de {(temperatura * 9/5) + 32} °F")
