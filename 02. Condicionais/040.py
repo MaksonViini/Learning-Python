1136cdb07495ef6c6ef1502980561ad331b03005
MaksonViini <maksonvinicio7@gmail.com>
2021-02-21 13:48:53 -0300

    Conditional

--- 02. Condicionais/040.py
+++
@@ -1,9 +0,0 @@
-# Aquele clássico da Média
-n1 = float(input("Primeira nota: "))
-n2 = float(input("Segunda nota: "))
-media = (n1 + n2) / 2
-print(f"A media do aluno e {media}")
-if media >= 6:
-    print("Aprovado!!!")
-else:
-    print("Reprovado")
