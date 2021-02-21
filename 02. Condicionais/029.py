3991dc2169c05c99bb29fea55a50656681c064d6
MaksonViini <maksonvinicio7@gmail.com>
2021-02-21 13:47:45 -0300

    Delete 029.py

--- 02. Condicionais/029.py
+++
@@ -1,7 +0,0 @@
-#Radar eletrônico
-velocidade = int(input("Entre com sua velocidade atual em Km/h: "))
-if velocidade >= 80:
-    print("VOCE FOI MULTADO !!")
-    print(f"Voce excedeu o limite de 80Km/h em {velocidade - 80}Km/h e foi multado em R$ {(velocidade - 80) * 7} ")
-else:
-    print("Boa Viagem !!!")
