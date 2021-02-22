374b4d8d97bad7aa0a33ad0c325846b7712ca6d6
MaksonViini <maksonvinicio7@gmail.com>
2021-02-22 13:38:42 -0300

    Create 033.py

---
+++ 02. Conditional/033.py
@@ -0,0 +1,6 @@
+# Maior e menor valores
+n1 = int(input("Informe um valor: "))
+n2 = int(input("Informe um valor: "))
+n3 = int(input("Informe um valor: "))
+print(f"O menor valor e {min(n1, n2, n3)}")
+print(f"O maior valor e {max(n1, n2, n3)} ")
